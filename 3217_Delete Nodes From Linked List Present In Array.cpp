c8928fbc7c08034fa2297d6b88d02c57837bf138
Arpita Kukreja <160055054+arpita-kukreja@users.noreply.github.com>
2025-11-01 23:22:45 +0530

    Implement function to delete nodes from linked list

---
+++ 3217_Delete Nodes From Linked List Present In Array.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        ListNode* curr=head;
+        ListNode* prev;
+        while(curr!=NULL){
+            if (s.count(curr->val)){
+                if(curr==head){
+                    head=head->next;
+                }
+                else prev->next=curr->next;
+            }
+            else{
+                prev=curr;
+            }
+            
+            curr=curr->next;
+        }
+        return head;
+    }
+};
