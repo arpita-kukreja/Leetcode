5667f31bf23fe781ccbbc1b0706b583f9d6bcffa
Arpita Kukreja <160055054+arpita-kukreja@users.noreply.github.com>
2025-11-03 23:24:25 +0530

    Add solution for minimum time to make rope colorful
    Implement minCost function to calculate minimum cost to make rope colorful.

---
+++ 1578_Minimum_time_to_make_rope_colorful.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int ans=0;
+        for(int i=1;i<colors.length();i++){
+            if(colors[i]==colors[i-1]){
+                if(neededTime[i]<neededTime [i-1]){
+                    ans+=neededTime[i];
+                    neededTime[i]=neededTime[i-1];
+                }
+                else{
+                    ans+=neededTime[i-1];
+                    neededTime[i-1]=neededTime[i];
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
